c6bdca12cd2ff735ea674ba856ad72f03a05d0af
Prabhakaran-Gokul <prabhakaran.gokul@gmail.com>
2023-11-25 02:43:16 -0500

    Add functionality for path planner to accept a list of waypoints

---
+++ L-MPC/cvxfile.m
@@ -0,0 +1,9 @@
+%% Add CVX Library 
+function cvxfile()
+    addpath("C:\Users\prabh\OneDrive\Documents\cvx\")
+    addpath("C:\Users\prabh\OneDrive\Documents\cvx\structures\")
+    addpath("C:\Users\prabh\OneDrive\Documents\cvx\lib\")
+    addpath("C:\Users\prabh\OneDrive\Documents\cvx\functions\")
+    addpath("C:\Users\prabh\OneDrive\Documents\cvx\commands\")
+    addpath("C:\Users\prabh\OneDrive\Documents\cvx\builtins\")
+end
